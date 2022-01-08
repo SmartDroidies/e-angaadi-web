69aff6ba38b4b5777c463aa54a421eed8d3d9982
baranirajeshbabu30 <cheersbarani30@gmail.com>
2022-01-08 19:09:23 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './core/home/home.component';
+import { NavmenuComponent } from './core/navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavmenuComponent,
   ],
   imports: [
     BrowserModule,
