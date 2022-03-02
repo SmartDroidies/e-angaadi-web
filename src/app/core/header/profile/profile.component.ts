bf4fddd47f2c513b8641ef1729f355b2d3cbbc73
kumarindu <kumarindu97@gmail.com>
2022-03-02 07:28:19 +0530

    sign in

--- src/app/core/header/profile/profile.component.ts
+++ src/app/core/header/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-profile',
@@ -7,12 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  mail = '';
+  name = '';
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.initUser();
   }
-  onSignin(): void {
-    this.router.navigate(['/review']);
+  initUser() {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.mail = user.attributes.email;
+        this.name = user.attributes.user_name;
+      })
   }
 }
