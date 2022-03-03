4d4c647a07ce8d0c4029108be0722ee8380b675a
Jagan <jagannarayanan245@gmail.com>
2022-03-03 15:24:14 +0530

    new-components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductListingComponent } from './components/product-listing/product-listing.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CategoryComponent, ProductListingComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
