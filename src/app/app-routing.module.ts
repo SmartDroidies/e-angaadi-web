ba892f059181156ca5d897f14fc3bd40485b68d7
kumarindu <kumarindu97@gmail.com>
2022-03-03 07:33:15 +0530

    hide header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './core/component/signin/signin.component';
+import { ProfileComponent } from './core/header/profile/profile.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
-  { path: 'signin', component: SigninComponent, pathMatch: 'full' }
+  { path: 'signin', component: SigninComponent, pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full' },
+ 
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
 import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
+import { SigninComponent } from './core/component/signin/signin.component';
+import { ProfileComponent } from './core/header/profile/profile.component';
+import { HomeComponent } from './core/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +13,11 @@ export class AppComponent implements OnInit {
   title = 'E-Angaadi';
   // user: CognitoUserInterface | undefined;
   // authState!: AuthState;
-
+  isShow=true;
+  showHideNav(event: any){
+        this.isShow= !(event instanceof SigninComponent);                                        
+                               
+  } 
   constructor() {}
 
   ngOnInit() {
