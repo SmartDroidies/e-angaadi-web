da0dee38b4b9696fa9100cfab5fc86006ba56c66
baranirajeshbabu30 <cheersbarani30@gmail.com>
2022-03-11 07:00:01 +0530

    dropdown

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,10 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { CartComponent } from './components/cart/cart.component';
 import { MatCardModule } from '@angular/material/card';
-
+import { ScrollingModule } from '@angular/cdk/scrolling';
 @NgModule({
   declarations: [CartComponent, CartSummaryComponent],
-  imports: [CommonModule, MatIconModule, MatCardModule],
+  imports: [CommonModule, MatIconModule, MatCardModule, ScrollingModule],
   exports: [CartComponent, CartSummaryComponent],
 })
 export class SharedModule {}
