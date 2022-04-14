713a59c1f8660fc1d1fff3a2b154cac0baebd4e3
Jagan <jagannarayanan245@gmail.com>
2022-04-14 11:22:05 +0530

    removed-comments

--- src/app/auth/pages/sign-in/sign-in.component.spec.ts
+++ src/app/auth/pages/sign-in/sign-in.component.spec.ts
@@ -2,20 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { SignInComponent } from './sign-in.component';
-import { Router } from '@angular/router';
 
 describe('SigninComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
-  let routerSpy = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SignInComponent],
       imports: [RouterTestingModule,AmplifyUIAngularModule],
-      providers: [
-        { provide: Router, useValue: routerSpy }
-      ]
     }).compileComponents();
   });
 
@@ -25,9 +20,4 @@ describe('SigninComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should navigate to home on signin', () => {
-  //   component.ngOnInit();
-  //   fixture.detectChanges();
-  //   expect (routerSpy.navigate).toHaveBeenCalledWith(['/home']);
-  // });
 });
