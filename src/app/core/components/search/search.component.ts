86d9b9ec86f290e66bc848866c6b622ba775368a
Jagan <jagannarayanan245@gmail.com>
2022-05-26 19:55:49 +0530

    search-api

--- src/app/core/components/search/search.component.ts
+++ src/app/core/components/search/search.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/product/models/product';
+import { ProductService } from 'src/app/product/service/product.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent { }
+
+export class SearchComponent {
+
+  search!: string;
+
+  constructor(private productService: ProductService) { }
+
+  searchData() {
+   this.productService.searchProduct(this.search).subscribe((search) => (this.search = search));
+  }
+}
