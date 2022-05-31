21c8dda00efd701ebd4913f4d69df3033a96b223
baranirajeshbabu30 <cheersbarani30@gmail.com>
2022-05-31 15:09:10 +0530

    cartstyle

--- src/app/cart/pages/cart-detail/cart-detail.component.ts
+++ src/app/cart/pages/cart-detail/cart-detail.component.ts
@@ -12,12 +12,12 @@ import { Router } from '@angular/router';
 export class CartDetailComponent implements OnInit {
   @Input() product!: Product;
   items: CartItem[] = [];
-  saved:any;
-  show!:boolean;
+  saved: any;
+  show!: boolean;
 
-  constructor(private cartService: CartService, private router: Router) {}
-  displayedColumns1: string[] = [ 'title',  'quantity', 'total'];
-  displayedColumns2: string[] = [ 'title',  'quantity', 'total'];
+  constructor(private cartService: CartService, private router: Router) { }
+  displayedColumns1: string[] = ['title', 'quantity', 'total'];
+  displayedColumns2: string[] = ['title', 'quantity', 'total'];
   ngOnInit(): void {
     this.getCart();
     this.showCart();
@@ -27,13 +27,13 @@ export class CartDetailComponent implements OnInit {
     this.items = this.cartService.getCartItems();
   }
 
-  showCart(){
-  if(this.items.length>0){
-    this.show=true;
-  }
-  else{
-    this.show=false;
-  }
+  showCart() {
+    if (this.items.length > 0) {
+      this.show = true;
+    }
+    else {
+      this.show = false;
+    }
   }
 
   addUnit(product: Product, selectedUnit: number) {
@@ -46,32 +46,32 @@ export class CartDetailComponent implements OnInit {
   }
 
   getSubTotal(cartItem: CartItem) {
-    let subTotal = 0;    
+    let subTotal = 0;
     this.items.forEach((loopItem) => {
-      if(loopItem.code === cartItem.code && loopItem.unit === cartItem.unit) {
+      if (loopItem.code === cartItem.code && loopItem.unit === cartItem.unit) {
         //FIXME - The price needs to be pulled from the service
-        subTotal = 50 * cartItem.quantity;    
+        subTotal = 50 * cartItem.quantity;
       }
     });
     return subTotal;
   }
 
-onSave(){
+  onSave() {
 
-}
-
-  emptyCart(){
-    return 
   }
 
-  getTotal(){
+  emptyCart() {
+    return
+  }
+
+  getTotal() {
     let total = 0;
     this.items.forEach((items) => {
       total += this.getSubTotal(items);
     });
     return total;
   }
-  
+
 
   async onShopping() {
     await this.router.navigate(['/home']);
