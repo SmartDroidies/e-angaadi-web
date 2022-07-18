1e444aae4f5fefb22621d76452b9ad37032e174f
Jagan <jagannarayanan245@gmail.com>
2022-07-18 07:03:00 +0530

    tracking-code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'E-Angaadi';
 
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, private router: Router) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('ta');
+  }
+
+  ngOnInit(): void {
+    this.setUpAnalytics();
+  }
+
+  setUpAnalytics() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-YOUR-GOOGLE-ID',
+          {
+            page_path: event.urlAfterRedirects
+          }
+        );
+      });
   }
 }
