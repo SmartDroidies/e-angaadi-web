76cfbef357d10acf749b66720213f958fdd0a81c
baranirajeshbabu30 <cheersbarani30@gmail.com>
2022-08-08 18:35:15 +0530

    saveforlater

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -8,8 +8,9 @@ import { CdkTableModule } from '@angular/cdk/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
+import { SaveLaterComponent } from './pages/save-later/save-later.component';
 @NgModule({
-  declarations: [CartDetailComponent],
+  declarations: [CartDetailComponent, SaveLaterComponent],
   imports: [CartRoutingModule, MatCardModule, CommonModule, MatTableModule, CdkTableModule, MatButtonModule,MatIconModule, TranslateModule],
 })
 export class CartModule {}
---
+++ src/app/cart/pages/save-later/save-later.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-later',
+  templateUrl: './save-later.component.html',
+  styleUrls: ['./save-later.component.scss']
+})
+export class SaveLaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
